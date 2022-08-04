5d4659452a78f4b3f23d898b4a3f804c84f0836d
kingrayhan <rayhan095@gmail.com>
2022-08-04 17:37:32 +0600

    wip:orders api

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { connect } from "mongoose";
 import products from "./products";
+import orders from "./orders";
 const app = express();
+
 dotenv.config({
   path: ".env",
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connect(process.env.DATABASE_URL as string)
   .then(() => {
@@ -21,6 +26,7 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 app.use("/products", products);
+app.use("/orders", orders);
 
 const port = process.env.PORT || 2345;
 app.listen(port, () => {
